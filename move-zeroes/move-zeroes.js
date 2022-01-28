c7b78b683c2beb5c3012aed55728495a38429588
HH <36600885+bereal1995@users.noreply.github.com>
2022-01-28 13:24:02 +0900

    Time: 68 ms, Memory: 47.4 MB - LeetHub

--- move-zeroes/move-zeroes.js
+++ move-zeroes/move-zeroes.js
@@ -3,15 +3,30 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    let zeroCount = 0;
-    for(let i=0; i<nums.length; i++) {
-        if (nums[i] === 0) {
-            nums.splice(i, 1)
-            i--
-            zeroCount++
+    // old
+    // let zeroCount = 0;
+    // for(let i=0; i<nums.length; i++) {
+    //     if (nums[i] === 0) {
+    //         nums.splice(i, 1)
+    //         i--
+    //         zeroCount++
+    //     }
+    // }  
+    // for(let i=0; i<zeroCount; i++) {
+    //     nums.push(0)
+    // }
+    
+    // new
+    let left = 0;
+    let temp;
+    
+    for(let i = 0; i < nums.length ; i++){
+        if(nums[i] !== 0){
+            temp = nums[left];
+            nums[left] = nums[i];
+            nums[i] = temp;
+            
+            left++;
         }
-    }  
-    for(let i=0; i<zeroCount; i++) {
-        nums.push(0)
     }
 };
