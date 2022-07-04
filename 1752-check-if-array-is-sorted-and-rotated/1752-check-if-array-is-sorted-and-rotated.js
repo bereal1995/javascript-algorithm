d31f32c42905109cfbe217374308b796eaba9ed9
HH <36600885+bereal1995@users.noreply.github.com>
2022-07-04 11:08:24 +0900

    Time: 77 ms (56.84%), Space: 41.8 MB (67.37%) - LeetHub

---
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var check = function(nums) {
+    let count = 0;
+    let len = nums.length - 1;
+    for (let i = 0; i < len; i++) {
+        if (nums[i] > nums[i + 1]) {
+            count++;
+        }
+    }
+    if (count > 1 || (count == 1 && nums[0] < nums[len])) {
+        return false;
+    }
+    return true;
+};
