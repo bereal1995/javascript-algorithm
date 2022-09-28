0293f0d822a93bd30eca573ccbad28995eab19f8
HH <36600885+bereal1995@users.noreply.github.com>
2022-09-29 01:31:24 +0900

    Time: 113 ms (10.09%), Space: 41.6 MB (94.74%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.js
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.js
@@ -13,32 +13,12 @@ var middleNode = function(head) {
     let fastNode = head;
     let slowNode = head;
     
-    while(fastNode) {
-        if (fastNode.next) {
-            fastNode = fastNode.next.next;
-            slowNode = slowNode.next;
-        } else {
-            break;
-        }
+    while(fastNode?.next) {
+        fastNode = fastNode.next.next;
+        slowNode = slowNode.next;
     }
     
     return slowNode;
+    
+    
 };
-// var middleNode = function(head) {
-//     let count = 0;
-//     let current = head;
-    
-//     while(current) {
-//         current = current.next;
-//         count++;
-//     }
-//     current = head;
-//     const halfCount = parseInt(count/2);
-    
-//     for(let i=0; i<count; i++) {
-//         if (halfCount === i) {
-//             return current
-//         }
-//         current = current.next;
-//     }
-// };
