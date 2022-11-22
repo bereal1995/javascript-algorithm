d1d8cb78e16d57669489f69a0208c1c9bf93f32d
HH <36600885+bereal1995@users.noreply.github.com>
2022-11-23 08:57:07 +0900

    Time: 85 ms (96.86%), Space: 49.8 MB (70.70%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
@@ -2,11 +2,12 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findDuplicate = function(nums) {
-    for(let i=0; i<nums.length; i++) {
-        const value = Math.abs(nums[i]);
-        if (nums[value] < 0) return value;
-        nums[value] = -nums[value];
-    }
-    return 0
+var findDuplicate = function (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    const index = Math.abs(nums[i]);
+
+    if (nums[index] < 0) return index;
+    nums[index] = -nums[index];
+  }
+  return 0;
 };
